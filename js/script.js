5898bcb4cb5d1d2773237af418b257fb9d1fd89e
ar4ibais <ak.hutov@yandex.ru>
2023-07-10 17:18:34 +0400

    2 part done

--- js/script.js
+++ js/script.js
@@ -7,10 +7,23 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', '');
-const b = prompt('На сколько оцените его?', '');
-const c = prompt('Один из последних просмотренных фильмов?', '');
-const d = prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', '');
+    const b = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if (a != null && b != null && b.length <= 50 && a != '' && b != '') {
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Мало');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Чел хорош');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Мега хорош');
+} else {
+    console.log('мяу');
+}
